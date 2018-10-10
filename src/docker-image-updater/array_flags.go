6466f119ca75b0e485ad4598c9da3a978a096e47
Rasmus Munk <munk1@live.dk>
2018-10-10 13:46:44 +0200

    Missing protection of images to be updated

--- src/docker-image-updater/array_flags.go
+++ src/docker-image-updater/array_flags.go
@@ -2,13 +2,14 @@ package main
 
 import "fmt"
 
-type arrayFlags []string
+type arrayFlags map[string]struct{}
 
 func (i *arrayFlags) String() string {
 	return fmt.Sprintf("%v", *i)
 }
 
 func (i *arrayFlags) Set(Value string) error {
-	*i = append(*i, Value)
+
+	*i["sdfdsf"]struct {}{}
 	return nil
 }
--- src/docker-image-updater/updater.go
+++ src/docker-image-updater/updater.go
@@ -12,32 +12,58 @@ import (
 )
 
 func updateImage(ctx context.Context, client *client.Client, image string) (error) {
-	readio, err := client.ImagePull(ctx, image, types.ImagePullOptions{})
+	readIo, err := client.ImagePull(ctx, image, types.ImagePullOptions{})
 	if err != nil {
 		return err
 	}
 
-	if readio != nil {
+	if readIo != nil {
 		// Get output
 		buf := new(bytes.Buffer)
-		buf.ReadFrom(readio)
+		buf.ReadFrom(readIo)
 		fmt.Printf("%s update response: %s", image, buf.String())
 	}
 
 	return nil
 }
 
-func run() {
-	var imageFlags arrayFlags
-	var updateInterval int
+func removeImage(ctx context.Context, client *client.Client, image types.ImageSummary) (error) {
+	delResp, err := client.ImageRemove(ctx, image.ID, types.ImageRemoveOptions{Force:true, PruneChildren:true})
+	if err != nil {
+		return err
+	}
+	for _, d := range delResp {
+		fmt.Printf("Delete response %s untagged response %s", d.Deleted, d.Untagged)
+	}
+	return nil
+}
 
-	flag.Var(&imageFlags, "update", "A list of images that should be monitored for update pulls")
-	flag.IntVar(&updateInterval, "interval", 10, "How often should the service check for image updates in minutes")
-	flag.Bool("prune", false, "Whether non listed image should be pruned/removed from the host")
-	flag.Bool("keep_untagged", true, "A flag on whether untagged/nontagged images should be kept on the host")
+
+func hostImages(ctx context.Context, client *client.Client) ([]types.ImageSummary, error ){
+	images, err := client.ImageList(ctx, types.ImageListOptions{})
+	if err != nil {
+		return nil, err
+	}
+	return images, nil
+}
+
+
+func run() {
+	var updateImages arrayFlags
+	var interval int
+	var prune bool
+	var keepUntagged bool
+
+	flag.Var(&updateImages, "update", "A list of images that should be monitored for update pulls")
+	flag.IntVar(&interval, "interval", 10,
+		"How often should the service check for image updates in minutes")
+	flag.BoolVar(&prune, "prune", false,
+		"Whether non listed images should be pruned/removed from the host")
+	flag.BoolVar(&keepUntagged,"keep_untagged", true,
+		"A flag on whether untagged/nontagged images should be kept on the host")
 	flag.Parse()
 
-	if imageFlags.String() == "[]" {
+	if updateImages.String() == "[]" {
 		flag.PrintDefaults()
 		os.Exit(1)
 	}
@@ -47,16 +73,35 @@ func run() {
 	if err != nil {
 		panic(err)
 	}
-
 	for {
-		fmt.Println("Checking for new images",
-			time.Now().Format(time.RFC3339))
-		for _, i := range imageFlags {
-			fmt.Printf("Checking %s for a new version \n", i)
-			if err := updateImage(ctx, cli, i); err != nil {
-				fmt.Printf("Failed to check %s for updates, (err): %s \n", i, err)
+		fmt.Println("Monitor stage start ", time.Now().Format(time.RFC3339))
+		fmt.Printf("Prune nonlisted images %v \n", prune)
+
+		if prune {
+			images, err := hostImages(ctx, cli)
+			if err != nil {
+				fmt.Printf("Failed to retrieve host images, (err): %v \n", err)
+			}
+
+			for _, i := range images {
+
+
+
+				if err := removeImage(ctx, cli, i); err != nil {
+					fmt.Printf("Failed to remove %v, (err): %v \n", i.ID, err)
+				}
 			}
 		}
-		time.Sleep(time.Duration(updateInterval) * time.Minute)
+
+		fmt.Println("Checking for new images", time.Now().Format(time.RFC3339))
+		for _, i := range updateImages {
+			fmt.Printf("Checking %v for a new version \n", i)
+			if err := updateImage(ctx, cli, i); err != nil {
+				fmt.Printf("Failed to check %v for updates, (err): %v \n", i, err)
+			}
+		}
+
+		fmt.Println("Monitor stage finished ", time.Now().Format(time.RFC3339))
+		time.Sleep(time.Duration(interval) * time.Minute)
 	}
 }
